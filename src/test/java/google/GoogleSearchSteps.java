00aae68945ce90695d5325a66ac231c6ff090093
cuongtv <mr.cuongtv@yahoo.com>
2021-03-19 10:00:51 +0700

    update assertion for testrun results

--- src/test/java/google/GoogleSearchSteps.java
+++ src/test/java/google/GoogleSearchSteps.java
@@ -1,5 +1,7 @@
 package google;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -82,5 +84,6 @@ public class GoogleSearchSteps {
 		} else {
 			log.pass("Found [" + count + "] result(s).");
 		}
+		assertTrue(count > 0);
 	}
 }
